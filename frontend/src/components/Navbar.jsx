cfccce76169a1e3fe28ce81e4138413ed35bdba3
samiyariya <samiya.riya16@gmail.com>
2024-09-29 22:16:16 +0600

    54:00

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -36,10 +36,21 @@ const Navbar = () => {
         <div className='flex items-center gap-4'>   {/*button will be hidden in phone view, visible in medium & large devices */}
             {
                 token   // if token is true then user is logged in & hide button
-                ? <div>   {/*profile pic will be shown as user is logged in*/}
+                // group class is used on the parent div, when parent div is hovered over, the child element can respond making it visible
+                ? <div className='flex items-center gap-2 cursor-pointer group relative'>   {/*profile pic will be shown as user is logged in*/}
                     <img className='w-8 rounded-full' src={assets.profile_pic} alt=''/>
                     <img className='w-2.5' src={assets.dropdown_icon} alt=''/>
+                    {/* This div is hidden by default but becomes visible we hover over image (which has the group class)*/}
+                    <div className='absolute top-0 right-0 pt-14 text-base font-medium text-gray-600 z-20 hidden group-hover:block'>
+                        <div className='min-w-48 bg-stone-100 rounded flex flex-col gap-4 p-4'>
+                            <p onClick={()=>navigate('/my-profile')} className='hover:text-black cursor-pointer'>My Profile</p>
+                            <p onClick={()=>navigate('/my-appointments')} className='hover:text-black cursor-pointer'>My Appointments</p>
+                            {/* after clicking on logout, token will be false &login button will be visible */}
+                            <p onClick={()=>setToken(false)} className='hover:text-black cursor-pointer'>Logout</p>
+                        </div>
+                    </div>
                 </div> 
+
                 // token false, show button, onClick will navigate to login page
                 : <button onClick={()=>navigate('/login')} className='bg-primary text-white px-8 py-3 rounded-full font-light hidden md:block'>Create Account</button>
             }
