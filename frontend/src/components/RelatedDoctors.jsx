5520db5a0c674c7cdfbd06d5eb183ed1a11b983f
samiyariya <samiya.riya16@gmail.com>
2024-10-27 22:29:09 +0600

    3:00:23, though some errors in appointment.jsx, appointment booking time 2:36.

---
+++ frontend/src/components/RelatedDoctors.jsx
@@ -0,0 +1,21 @@
+import React, { useContext, useEffect, useState } from 'react'
+import { AppContext } from '../context/AppContext'
+
+const RelatedDoctors = (speciality, docId) => {
+
+    const {doctors} = useContext(AppContext)
+    const [relDoc, setRelDocs] = useState([])
+
+    useEffect(()=>{
+        if(doctors.length > 0 && speciality) {
+            const doctorsData = doctors.filter((doc)=> doc.speciality === speciality && doc._id !== docId)
+            setRelDocs(doctorsData)
+        }
+    },[doctors, speciality, docId])
+
+  return (
+    <div>RelatedDoctors</div>
+  )
+}
+
+export default RelatedDoctors
