233187ad19886aab94c7e71a24f01e569be5f669
samiyariya <samiya.riya16@gmail.com>
2024-11-15 13:45:34 +0600

    4:35:00

--- frontend/src/pages/MyAppointments.jsx
+++ frontend/src/pages/MyAppointments.jsx
@@ -9,7 +9,7 @@ const MyAppointments = () => {
     <div>
       <p className='pb-3 mt-12 font-medium text-zinc-700 border-b'>My appointments</p>
       <div>
-        {doctors.slice(0,2).map((item, index)=>(
+        {doctors.slice(0,3).map((item, index)=>(
           <div className='grid grid-cols-[1fr_2fr] gap-4 sm:flex sm:gap-6 py-2 border-b' key={index}>
             <div>
               <img className='w-32 bg-indigo-50' src={item.image} alt=""/>
