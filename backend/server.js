f63fa3f3dfeec12e38ce15e64e440269cc2523ab
samiyariya <samiya.riya16@gmail.com>
2024-11-18 13:58:00 +0600

    5:00:24

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,23 @@
 import express from 'express'
+import cors from 'cors'
+import 'dotenv/config'
+import connectDB from './config/mongodb.js'
+import connectCloudinary from './config/cloudinary.js'
+
+
+// app config
+const app = express()
+const port = process.env.PORT || 4000
+connectDB()
+connectCloudinary()
+
+// middlewares
+app.use(express.json())   // request wil get passed through this method
+app.use(cors())           // connect frontend to backend
+
+// api endpoint
+app.get('/',(req, res)=>{
+    res.send('API WORKING CORRECTLY!!!!')
+})
+
+app.listen(port, () => console.log("Server is running on port", port))
