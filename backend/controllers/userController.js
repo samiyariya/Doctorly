4bb6b921802243d827f9b1e9ad4c8b508959c439
samiyariya <samiya.riya16@gmail.com>
2024-12-19 16:44:14 +0600

    9:44:00

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -2,6 +2,8 @@ import validator from 'validator'
 import bcrypt from 'bcrypt'
 import userModel from '../models/userModel.js'
 import jwt from 'jsonwebtoken'
+import { v2 as cloudinary } from "cloudinary"
+ 
 
 // API to register user 
 const registerUser = async(req, res) => {
@@ -103,7 +105,26 @@ const getProfile = async(req, res) => {
 const updateProfile = async(req, res) => {
     try {
 
+        const {userId, name, phone, address, dob, gender} = req.body
+        const imageFile = req.file
+
+        if(!name || !phone || !address || !dob || !gender){
+            return res.json({succcess: false, message: "Missing Details"})
+        }
+
+        await userModel.findByIdAndUpdate(userId, {name, phone, address: JSON.parse(address), dob, gender})
         
+        if(imageFile){
+            // uploading image to cloudinary
+            const imageUpload = await cloudinary.uploader.upload(imageFile.path, {resource_type: "image"})
+            const imageUrl = imageUpload.secure_url
+
+            await userModel.findByIdAndUpdate(userId, {image: imageUrl})
+
+            res.json({success: true, message: "Profile Updated"})
+        }
+
+
     } catch (error) {
         console.log(error)
         res.json({success: false, message: error.message})    
