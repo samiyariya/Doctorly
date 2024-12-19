251510f3410ffa8fcaa553ff216c069fe17bbb53
samiyariya <samiya.riya16@gmail.com>
2024-12-19 16:33:00 +0600

    9:35:00 -> getprofile api created

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -1,10 +1,12 @@
 import express from 'express'
-import { registerUser } from '../controllers/userController.js'
-import { loginUser } from '../controllers/userController.js' 
+import { getProfile, loginUser, registerUser } from '../controllers/userController.js'
+import authUser from '../middlewares/authUser.js'
 
 const userRouter = express.Router()
 
 userRouter.post('/register', registerUser)
 userRouter.post('/login', loginUser)
+
+userRouter.get('/profile', authUser, getProfile)
 
 export default userRouter
